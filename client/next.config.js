14a8f88c656b3575cab886d706be31934c8b64c2
Tudor Constantin <tudorconstantin@gmail.com>
2022-01-28 10:14:50 +0200

    Fresj new NextJS app

--- client/next.config.js
+++ client/next.config.js
@@ -1,25 +1,6 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = (phase, { defaultConfig }) => {
-  // if (phase === PHASE_DEVELOPMENT_SERVER) {
-  //   return {
-  //     /* development only config options here */
-  //   }
-  // }
-
-  return {
-    /* config options for all phases except development here */
-    distDir: 'dist',
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      // Note: we provide webpack above so you should not `require` it
-      // Perform customizations to webpack config
-      // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-
-      if (process.env.NODE_ENV === 'test') {
-        // config.devtool = 'inline-source-map';
-      }
-      // Important: return the modified config
-      return config;
-    },
-  };
-};
+module.exports = nextConfig
