e379fa70efb28871fbe7ad2e5916e68c31704d8c
tudorconstantin <tudorconstantin@gmail.com>
2022-01-29 17:09:51 +0200

    WiP

--- e2e_tests/docker/entrypoint.sh
+++ e2e_tests/docker/entrypoint.sh
@@ -3,6 +3,7 @@ echo "Checking client is up ($CLIENT_URL) ..."
 for i in $(seq 1 30); do
     if curl -s --head  --request GET "$CLIENT_URL/signup" | grep "200 OK" > /dev/null; then 
       echo "$CLIENT_URL is UP"
+      sleep 5 #after the client is reported to have started, it takes a few more seconds for it to report "event - compiled client and server successfully"
       break
     else
       echo $(date -u) "$CLIENT_URL is DOWN ... retrying" && sleep 1
